547a741c5fb689bea77ba66d899f4c43bc524a1a
Yummy <yummy@X61T.(none)>
2012-12-04 15:34:23 +0800

    once again, restart...

---
+++ demo/qwproblem/testnull.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+use strict;
+print "test null.\n";
+my $str="";
+if(!$str){
+	print "str is empty , and its same as null!\n";
+}else{
+	print "str is empty , and not same as null!\n";
+}
